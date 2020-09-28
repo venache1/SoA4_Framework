68515f01870361c475bb6650ddedf6a619135c00
Claudia-20 <claudia.movradin@gmail.com>
2020-09-28 11:37:40 +0300

    Cucumber Runner. Closes #23

---
+++ src/test/java/com/endava/soa4/RunCucumberTest.java
@@ -0,0 +1,14 @@
+package com.endava.soa4;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "resources/feature",
+        glue = {"java/com/endava/soa4/stepDefinition"},
+        tags = {"@Claudia or @Anatolie or @Irina or @Stefan or @Artiom ", "@run"})
+public class RunCucumberTest {
+
+}
